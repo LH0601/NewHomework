efee873cad0ff591453f88e3a778e83c89872ca8
LH0601 <40553396+LH0601@users.noreply.github.com>
2019-05-04 12:46:45 +0800

    randomly
    okfine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'',redirectTo:'/home',pathMatch:"full"}
+  ,{path:'home',component:HomeComponent}
+  ,{path:'news',component:NewsComponent}
+  ,{path:'contact',component:ContactComponent}
+  ,{path:'about',component:AboutComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+   
+    HomeComponent,
+    NewsComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
